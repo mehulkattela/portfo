db25624b8ab3acb6c0fe6caea8ca786832bff531
MEHUL KATTELA <mehul.kattela@gmail.com>
2020-07-10 13:40:50 +0530

    My Portfolio WebSite

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_portfolio():
+    return render_template('index.html')
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\nemail:{email}, subject:{subject}, message:{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return render_template('thankyou.html')
+    else:
+        return 'Something went wrong, Try again!!'
